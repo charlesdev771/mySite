7827fb703483a4aa1b4cb3374264ae663deb92ee
charlesdev771 <charlesdev0721@protonmail.com>
2022-07-26 21:39:42 -0300

    Add Home

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+
+export default function Home()
+{
+    return(
+        <div id="HOME" class="HOME">
+            <div id="home" class="home">
+                <section id="section_home" class="section_home">
+                    <div class="">
+                        <h1 class="">Charles Dantas</h1>
+                        <h3 class="">Programador && Hacker Ético</h3>
+                    </div>
+                </section>
+            </div>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Navbar from './components/Navbar.js';
+import Home from './components/Home.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,9 +11,6 @@ root.render(
   <React.StrictMode>
     <App />
     <Navbar />
+    <Home />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
